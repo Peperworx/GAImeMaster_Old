d028b54552f289b70e6413cdd89c3bebaeed244b
Riley Wilton <meetingprince34@outlook.com>
2019-04-30 17:42:56 -0400

    Started implementation of sqlite 3
    Added sqlite3 database to login page. Player creation work in progress.

--- login.py
+++ login.py
@@ -10,26 +10,31 @@ import os
 import random
 import hashlib
 import subprocess
-try:
-	import boto3
-except:
-	subprocess.check_call([sys.executable, '-m', 'pip', "install", "boto3"])
+import sqlite3
+def connectUsers():
+    mydb = sqlite3.connect('data/users.sqlite')
+    return mydb
+def initdbUsers():
+    cnn = connectUsers()
+    cnnc = cnn.cursor()
+    cnnc.execute("CREATE TABLE IF NOT EXISTS Users (id INTEGER NOT NULL PRIMARY KEY,username TEXT NOT NULL, password TEXT NOT NULL, role INTEGER DEFAULT 0, email TEXT NOT NULL, friends TEXT DEFAULT [], characters TEXT DEFAULT [])")
+    cnn.close()
+
+initdbUsers()
 for _name in ('stdin', 'stdout', 'stderr'):
     if getattr(sys, _name) is None:
         setattr(sys, _name, open(os.devnull, 'r' if _name == 'stdin' else 'w'))
 del _name
 form = cgi.FieldStorage()
-dynamodb = boto3.resource('dynamodb', aws_access_key_id="AKIA3QPMHYLWUEZOGRW4", aws_secret_access_key="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", region_name="us-east-1")
-table = dynamodb.Table('Users')
 roles = {"user":0,"dk":1,"dm":2,"admin":3}
 def success(item):
     if "redirect" not in form:
-        if item["role"] > 0:
+        if item[3] > 1:
             print ("<html><body>\n")
             print ("<meta http-equiv=\"refresh\" content=\"0; url = http://"+os.environ["HTTP_HOST"]+"/admin\" />")
             print ("</body></html>")
             
-        elif item["role"] < 1:
+        elif item[3] <= 1:
             print ("<html><body>\n")
             print ("<meta http-equiv=\"refresh\" content=\"0; url = http://"+os.environ["HTTP_HOST"]+"/play\" />")
             print ("</body></html>")
@@ -42,7 +47,6 @@ def success(item):
         print ("<meta http-equiv=\"refresh\" content=\"0; url = "+form["redirect"].value+" \/>")
         print ("</body></html>")
 
-
 if "HTTP_COOKIE" not in os.environ:
     os.environ["HTTP_COOKIE"] = ""
 
@@ -50,14 +54,14 @@ if "login" in os.environ["HTTP_COOKIE"]:
     cookie = cookies.SimpleCookie(os.environ["HTTP_COOKIE"])
     uname = cookie["username"].value
     psswd = cookie["password"].value
-    response = table.get_item(
-            Key = {
-                    'username':uname
-                }
-        )
+    cnn = connectUsers()
+    cnnc = cnn.cursor()
+    cnnc.execute("SELECT * FROM Users WHERE username = ?", (uname,))
+    result = cnnc.fetchall()
+    cnn.close()
     try:
-        item = response["Item"]
-        if item["password"] == psswd:
+        item = result[0]
+        if item[2] == psswd:
             print("Content-Type:text/html")
             print("")
             success(item)
@@ -90,14 +94,14 @@ elif "uname" not in form or "psswd" not in form:
     print("")
     print(open("loginFailed.html", "r").read())
 else:
-    response = table.get_item(
-            Key = {
-                    'username':form["uname"].value
-                }
-        )
+    cnn = connectUsers()
+    cnnc = cnn.cursor()
+    cnnc.execute("SELECT * FROM Users WHERE username = ?", (form["uname"].value,))
+    result = cnnc.fetchall()
+    cnn.close()
     try:
-        item = response["Item"]
-        if item["password"] == hashlib.sha3_256(str(form["psswd"].value).encode()).hexdigest():
+        item = result[0]
+        if item[2] == hashlib.sha3_256(str(form["psswd"].value).encode()).hexdigest():
             print("Content-Type:text/html")
             cookie = cookies.SimpleCookie()
             cookie["session"] = random.randint(0,1000000000)
